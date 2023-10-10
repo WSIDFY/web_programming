d9c8541ded460d5f100d836aa26b6e12eb344166
WSIDFY <141166836+WSIDFY@users.noreply.github.com>
2023-10-10 23:49:36 +0900

    Add files via upload

---
+++ 4week-1/practice_1/6-1.js
@@ -0,0 +1,8 @@
+function over(obj)  // this 사용시 오류 발생으로 인해 매개변수인 obj를 통해 Dom요소에 접근
+{
+    obj.style.background="yellow";  // this 사용시 오류 발생으로 인해 매개변수인 obj를 통해 Dom요소에 접근
+}
+function out(obj)
+{
+  obj.style.background="white";
+}
